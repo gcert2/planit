237b31a10d8dedbe689371268b888d7583bd357a
planit <planit@tanjeffreyz.github.io>
2024-09-09 06:45:59 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,34 @@
-assignments = {};
+assignments = {
+  "ME 3293": [
+    {
+      "title": "Homework Assignment #1: Chapter 1",
+      "course": "ME 3293",
+      "dueDate": "2024-09-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/851677/assignments/4867628/submissions/265138252",
+      "submitted": true
+    },
+    {
+      "title": "Homework Assignment #2: Chapter 2",
+      "course": "ME 3293",
+      "dueDate": "2024-09-13T23:59:00",
+      "link": "https://www.gradescope.com/courses/851677",
+      "submitted": false
+    }
+  ],
+  "ME-3113-0C1-202510": [
+    {
+      "title": "Laboratory Assignment #1 - Measurement Devices",
+      "course": "ME-3113-0C1-202510",
+      "dueDate": "2024-09-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/826306/assignments/4749431/submissions/265378127",
+      "submitted": true
+    },
+    {
+      "title": "Homework #1",
+      "course": "ME-3113-0C1-202510",
+      "dueDate": "2024-09-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/826306",
+      "submitted": false
+    }
+  ]
+};
