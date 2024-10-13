306fc01b29d1619e8b0aea7ef07476f39a812e02
planit <planit@tanjeffreyz.github.io>
2024-10-13 17:37:02 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -62,8 +62,8 @@ assignments = {
       "title": "Laboratory Assignment #3 - Individual Peer Evaluation",
       "course": "ME-3113-0C1-202510",
       "dueDate": "2024-10-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/826306/assignments/4749435/submissions/277454642",
+      "submitted": true
     },
     {
       "title": "Homework #1",
