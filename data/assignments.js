434d93a55318249fb227602b56977fb4e481bc5a
planit <planit@tanjeffreyz.github.io>
2024-12-02 04:44:13 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -53,8 +53,8 @@ assignments = {
       "title": "Homework Assignment #8: Chapters 6 & 9",
       "course": "ME 3293",
       "dueDate": "2024-12-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/851677/assignments/4867640/submissions/293721912",
+      "submitted": true
     },
     {
       "title": "Certificate of Meet Your Faculty Mentor Event (10-25-2024)",
