f8d8f5559fcdef2096e6528b6a0f94b9e5ce7052
planit <planit@tanjeffreyz.github.io>
2025-01-01 02:10:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,70 +3,70 @@ assignments = {
     {
       "title": "Homework Assignment #1: Chapter 1",
       "course": "ME 3293",
-      "dueDate": "2024-09-06T23:59:00",
+      "dueDate": "2025-09-06T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867628/submissions/265138252",
       "submitted": true
     },
     {
       "title": "Homework Assignment #3: Chapter 2",
       "course": "ME 3293",
-      "dueDate": "2024-09-18T23:59:00",
+      "dueDate": "2025-09-18T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867630/submissions/269004770",
       "submitted": true
     },
     {
       "title": "Homework Assignment #2: Chapter 2",
       "course": "ME 3293",
-      "dueDate": "2024-09-13T23:59:00",
+      "dueDate": "2025-09-13T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867631/submissions/267242379",
       "submitted": true
     },
     {
       "title": "Homework Assignment #4: Chapter 3",
       "course": "ME 3293",
-      "dueDate": "2024-10-09T23:59:00",
+      "dueDate": "2025-10-09T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867633/submissions/276412093",
       "submitted": true
     },
     {
       "title": "Homework Assignment #6: Chapter 5",
       "course": "ME 3293",
-      "dueDate": "2024-11-05T23:59:00",
+      "dueDate": "2025-11-05T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867634/submissions/285958815",
       "submitted": true
     },
     {
       "title": "Homework Assignment #5: Chapter 4",
       "course": "ME 3293",
-      "dueDate": "2024-10-25T23:59:00",
+      "dueDate": "2025-10-25T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867635/submissions/282333487",
       "submitted": true
     },
     {
       "title": "Homework Assignment #9: Chapters 8 & 10",
       "course": "ME 3293",
-      "dueDate": "2024-12-05T23:59:00",
+      "dueDate": "2025-12-05T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867637/submissions/295089325",
       "submitted": true
     },
     {
       "title": "Homework Assignment #7: Chapter 6",
       "course": "ME 3293",
-      "dueDate": "2024-11-13T23:59:00",
+      "dueDate": "2025-11-13T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867638/submissions/288468977",
       "submitted": true
     },
     {
       "title": "Homework Assignment #8: Chapters 6 & 9",
       "course": "ME 3293",
-      "dueDate": "2024-12-01T23:59:00",
+      "dueDate": "2025-12-01T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/4867640/submissions/293721912",
       "submitted": true
     },
     {
       "title": "Certificate of Meet Your Faculty Mentor Event (10-25-2024)",
       "course": "ME 3293",
-      "dueDate": "2024-11-03T23:59:00",
+      "dueDate": "2025-11-03T23:59:00",
       "link": "https://www.gradescope.com/courses/851677/assignments/5239927/submissions/283380508",
       "submitted": true
     }
@@ -75,168 +75,168 @@ assignments = {
     {
       "title": "Laboratory Assignment #1 - Measurement Devices",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-09-08T23:59:00",
+      "dueDate": "2025-09-08T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749431/submissions/265378127",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #2 - Data Analysis",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-09-22T23:59:00",
+      "dueDate": "2025-09-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749432/submissions/269982010",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #2 - Individual Peer Evaluation",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-09-22T23:59:00",
+      "dueDate": "2025-09-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749433/submissions/270195382",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #3 - Circuit Fundamentals",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-10-13T23:59:00",
+      "dueDate": "2025-10-13T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749434/submissions/277433373",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #3 - Individual Peer Evaluation",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-10-13T23:59:00",
+      "dueDate": "2025-10-13T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749435/submissions/277454642",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #4 - Individual Peer Evaluation",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-11-03T23:59:00",
+      "dueDate": "2025-11-03T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749446/submissions/285087148",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #4 - Op-Amps and Passive RC Filters",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-11-03T23:59:00",
+      "dueDate": "2025-11-03T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749447/submissions/285005467",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #5 - Individual Peer Evaluation",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-11-17T23:59:00",
+      "dueDate": "2025-11-17T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749448/submissions/290128888",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #5 - Strain Gages",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-11-17T23:59:00",
+      "dueDate": "2025-11-17T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749449/submissions/290138165",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #6 - Final Project",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-12-07T23:59:00",
+      "dueDate": "2025-12-07T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749451/submissions/295659708",
       "submitted": true
     },
     {
       "title": "Laboratory Assignment #6 - Individual Peer Evaluation",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-12-05T23:59:00",
+      "dueDate": "2025-12-05T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4749452/submissions/295658286",
       "submitted": true
     },
     {
       "title": "Homework #1",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-09-15T23:59:00",
+      "dueDate": "2025-09-15T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4750774/submissions/267665355",
       "submitted": true
     },
     {
       "title": "Homework #2",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-09-22T23:59:00",
+      "dueDate": "2025-09-22T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4807909/submissions/270184404",
       "submitted": true
     },
     {
       "title": "Homework #3",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-10-13T23:59:00",
+      "dueDate": "2025-10-13T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4870433/submissions/277413773",
       "submitted": true
     },
     {
       "title": "Homework #4",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-10-20T23:59:00",
+      "dueDate": "2025-10-20T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/4915857/submissions/280134633",
       "submitted": true
     },
     {
       "title": "Exam #1",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-09-25T20:00:00",
+      "dueDate": "2025-09-25T20:00:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/5043116/submissions/271436940",
       "submitted": true
     },
     {
       "title": "MAKEUP EXAM ",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-10-03T18:00:00",
+      "dueDate": "2025-10-03T18:00:00",
       "link": "https://www.gradescope.com/courses/826306",
       "submitted": false
     },
     {
       "title": "Homework #5",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-11-10T23:59:00",
+      "dueDate": "2025-11-10T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/5107724/submissions/287698489",
       "submitted": true
     },
     {
       "title": "MIDTERM #2",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-10-23T20:00:00",
+      "dueDate": "2025-10-23T20:00:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/5189465/submissions/281310461",
       "submitted": true
     },
     {
       "title": "Midterm #2 - MAKEUP",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-11-01T17:00:00",
+      "dueDate": "2025-11-01T17:00:00",
       "link": "https://www.gradescope.com/courses/826306",
       "submitted": false
     },
     {
       "title": "Homework #6",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-11-24T23:59:00",
+      "dueDate": "2025-11-24T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/5262418/submissions/292551162",
       "submitted": true
     },
     {
       "title": "Homework #7",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-12-01T23:59:00",
+      "dueDate": "2025-12-01T23:59:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/5336572/submissions/293744939",
       "submitted": true
     },
     {
       "title": "Final Exam",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-12-04T20:30:00",
+      "dueDate": "2025-12-04T20:30:00",
       "link": "https://www.gradescope.com/courses/826306/assignments/5406218/submissions/294636963",
       "submitted": true
     },
     {
       "title": "Final Exam - Sick Makeup",
       "course": "ME-3113-0C1-202510",
-      "dueDate": "2024-12-10T16:12:00",
+      "dueDate": "2025-12-10T16:12:00",
       "link": "https://www.gradescope.com/courses/826306",
       "submitted": false
     }
