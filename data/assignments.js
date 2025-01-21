ea49ce564a0f3abd8ef5246d25990c7c09ca2607
planit <planit@tanjeffreyz.github.io>
2025-01-21 17:38:09 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,245 +1,270 @@
 assignments = {
-  "ME 3293": [
+  "ME4293 Spr25": [
     {
-      "title": "Homework Assignment #1: Chapter 1",
-      "course": "ME 3293",
-      "dueDate": "2025-09-06T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867628/submissions/265138252",
-      "submitted": true
+      "title": "Ch7 1. Prob 7.017",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #3: Chapter 2",
-      "course": "ME 3293",
-      "dueDate": "2025-09-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867630/submissions/269004770",
-      "submitted": true
+      "title": "Ch7 2. Prob 7.022",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #2: Chapter 2",
-      "course": "ME 3293",
-      "dueDate": "2025-09-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867631/submissions/267242379",
-      "submitted": true
+      "title": "Ch7 3. Prob 7.031",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #4: Chapter 3",
-      "course": "ME 3293",
-      "dueDate": "2025-10-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867633/submissions/276412093",
-      "submitted": true
+      "title": "Ch7 4. Prob 7.040",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #6: Chapter 5",
-      "course": "ME 3293",
-      "dueDate": "2025-11-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867634/submissions/285958815",
-      "submitted": true
+      "title": "Ch7 5. Prob 7.050",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #5: Chapter 4",
-      "course": "ME 3293",
-      "dueDate": "2025-10-25T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867635/submissions/282333487",
-      "submitted": true
+      "title": "Ch7 6. Prob 7.057",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #9: Chapters 8 & 10",
-      "course": "ME 3293",
-      "dueDate": "2025-12-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867637/submissions/295089325",
-      "submitted": true
+      "title": "Ch7 7. Prob 7.072",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #7: Chapter 6",
-      "course": "ME 3293",
-      "dueDate": "2025-11-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867638/submissions/288468977",
-      "submitted": true
+      "title": "Ch8 1. Prob 8.004",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Homework Assignment #8: Chapters 6 & 9",
-      "course": "ME 3293",
-      "dueDate": "2025-12-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/4867640/submissions/293721912",
-      "submitted": true
+      "title": "Ch8 2. Prob 8.011",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     },
     {
-      "title": "Certificate of Meet Your Faculty Mentor Event (10-25-2024)",
-      "course": "ME 3293",
-      "dueDate": "2025-11-03T23:59:00",
-      "link": "https://www.gradescope.com/courses/851677/assignments/5239927/submissions/283380508",
-      "submitted": true
+      "title": "Ch8 3. Prob 8.014",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch8 4. Prob 8.019",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch8 5. Prob 8.025",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch8 6. Prob 8.037",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-02-20T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch9 1. Prob 9.003",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch9 2. Prob 9.015",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch9 3. Prob 9.017",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch9 4. Prob 9.030",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch9 5. Prob 9.032",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch9 6. Prob 9.042",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch9 7. Prob 9.057",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch10 1. Prob 10.004",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch10 2. Prob 10.014",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch10 3. Prob 10.018",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch10 4. Prob 10.024",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch10 5. Prob 10.027",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch10 6. Prob 10.029",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-04-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch12 1. Prob 12.020",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch12 2. Prob 12.030",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch12 3. Prob 12.033",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch12 4. Prob 12.043",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch12 5. Prob 12.049",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch13 1. Prob 13.002",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch13 2. Prob 13.008",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch13 3. Prob 13.009",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch13 4. Prob 13.015",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch13 5. Prob 13.023",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch13 6. Prob 13.027",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
+    },
+    {
+      "title": "Ch13 7. Prob 13.031",
+      "course": "ME4293 Spr25",
+      "dueDate": "2025-05-08T23:50:00",
+      "link": "https://www.gradescope.com/courses/944675",
+      "submitted": false
     }
-  ],
-  "ME-3113-0C1-202510": [
-    {
-      "title": "Laboratory Assignment #1 - Measurement Devices",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-09-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749431/submissions/265378127",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #2 - Data Analysis",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-09-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749432/submissions/269982010",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #2 - Individual Peer Evaluation",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-09-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749433/submissions/270195382",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #3 - Circuit Fundamentals",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-10-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749434/submissions/277433373",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #3 - Individual Peer Evaluation",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-10-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749435/submissions/277454642",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #4 - Individual Peer Evaluation",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-11-03T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749446/submissions/285087148",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #4 - Op-Amps and Passive RC Filters",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-11-03T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749447/submissions/285005467",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #5 - Individual Peer Evaluation",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-11-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749448/submissions/290128888",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #5 - Strain Gages",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-11-17T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749449/submissions/290138165",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #6 - Final Project",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-12-07T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749451/submissions/295659708",
-      "submitted": true
-    },
-    {
-      "title": "Laboratory Assignment #6 - Individual Peer Evaluation",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-12-05T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4749452/submissions/295658286",
-      "submitted": true
-    },
-    {
-      "title": "Homework #1",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-09-15T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4750774/submissions/267665355",
-      "submitted": true
-    },
-    {
-      "title": "Homework #2",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-09-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4807909/submissions/270184404",
-      "submitted": true
-    },
-    {
-      "title": "Homework #3",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-10-13T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4870433/submissions/277413773",
-      "submitted": true
-    },
-    {
-      "title": "Homework #4",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-10-20T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/4915857/submissions/280134633",
-      "submitted": true
-    },
-    {
-      "title": "Exam #1",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-09-25T20:00:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/5043116/submissions/271436940",
-      "submitted": true
-    },
-    {
-      "title": "MAKEUP EXAM ",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-10-03T18:00:00",
-      "link": "https://www.gradescope.com/courses/826306",
-      "submitted": false
-    },
-    {
-      "title": "Homework #5",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-11-10T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/5107724/submissions/287698489",
-      "submitted": true
-    },
-    {
-      "title": "MIDTERM #2",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-10-23T20:00:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/5189465/submissions/281310461",
-      "submitted": true
-    },
-    {
-      "title": "Midterm #2 - MAKEUP",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-11-01T17:00:00",
-      "link": "https://www.gradescope.com/courses/826306",
-      "submitted": false
-    },
-    {
-      "title": "Homework #6",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-11-24T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/5262418/submissions/292551162",
-      "submitted": true
-    },
-    {
-      "title": "Homework #7",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-12-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/5336572/submissions/293744939",
-      "submitted": true
-    },
-    {
-      "title": "Final Exam",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-12-04T20:30:00",
-      "link": "https://www.gradescope.com/courses/826306/assignments/5406218/submissions/294636963",
-      "submitted": true
-    },
-    {
-      "title": "Final Exam - Sick Makeup",
-      "course": "ME-3113-0C1-202510",
-      "dueDate": "2025-12-10T16:12:00",
-      "link": "https://www.gradescope.com/courses/826306",
-      "submitted": false
-    }
-  ],
-  "ME-3813-003-202510": []
+  ]
 };
