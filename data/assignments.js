bda1f3fd89fe2402e7970b156de38e1718afb820
planit <planit@tanjeffreyz.github.io>
2025-02-11 06:43:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -36,6 +36,13 @@ assignments = {
       "dueDate": "2025-02-07T23:59:00",
       "link": "https://www.gradescope.com/courses/969569/assignments/5712255/submissions/306828201",
       "submitted": true
+    },
+    {
+      "title": "Online Activity - 02/11",
+      "course": "ME-3663-002-202520",
+      "dueDate": "2025-02-11T15:00:00",
+      "link": "https://www.gradescope.com/courses/969569",
+      "submitted": false
     }
   ],
   "ME4293 Spr25": [
